bad38f78ebfc90f61527fac003ca6daeb4065080
PraveenRL <rlppraveen@gmail.com>
2020-08-29 12:27:37 +0530

    Understanding the children prop

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const person = (props) => { //props -> can be any name
     return (
-        <p>I'm a {props.name}, I'm {props.age} old</p>
+        <div>
+            <p>I'm a {props.name}, I'm {props.age} old</p>
+            <p>{props.children}</p>
+        </div>
     );
 }
 
