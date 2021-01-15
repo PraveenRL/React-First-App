219d988c050f3c53f38ae445b01d1bb0dac0f4b6
Praveen_RL <rlppraveen@gmail.com>
2021-01-15 09:04:59 +0530

    props children

--- src/App.js
+++ src/App.js
@@ -6,12 +6,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
           <Person name="Max" age="22" />
           <Person name="Manu" age="23">My Hobbies: Racing</Person>
           <Person name="Stephanie" age="24" />
-        </header>
       </div>
     );
     // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'React App'));
